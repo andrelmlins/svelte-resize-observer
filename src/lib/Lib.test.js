92f91306f7f67ce62a6a4f4e45a24d0b5214ee8c
André Lins <andrelucas01@hotmail.com>
2020-04-13 11:00:01 -0300

    Fix tests

--- src/lib/Lib.test.js
+++ src/lib/Lib.test.js
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/svelte";
 
-import SvelteResizeObserver from "./index.svelte";
+import SvelteResizeObserver from "./ResizeObserver.svelte";
 
 test("shows proper heading when rendered", () => {
   const { container } = render(SvelteResizeObserver);
